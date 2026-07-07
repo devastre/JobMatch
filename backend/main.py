d2b91b9607bbdbb009bbd2e415b804474b390aeb
devastre <100860903+devastre@users.noreply.github.com>
2026-07-07 10:57:32 +0200

    Include search router in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import auth
+from routers import auth, search
 from database import engine, Base
 
 app = FastAPI(
@@ -19,6 +19,7 @@ app.add_middleware(
 )
 
 app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
+app.include_router(search.router, prefix="/api/search", tags=["search"])
 
 @app.get("/")
 def read_root():
